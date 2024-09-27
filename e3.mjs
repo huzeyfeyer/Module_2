8eda6e1501dc579f503e6b2659c9ec0c648b8173
huzeyfeyer <huzeyfeyer1@gmail.com>
2024-09-27 10:30:05 +0200

    Update e3.mjs

--- e3.mjs
+++ e3.mjs
@@ -6,41 +6,24 @@ let month = await userInput.question('welke maand wil je leren? ');
 
 switch(month){
     case 'januari':
-        console.log( 'Januari heeft 31 dagen.')
+    case 'maart':
+    case 'mei':
+    case 'juli':
+    case 'augustus':
+    case 'oktober':
+    case 'december':
+        console.log('Er zijn 31 dagen in ' + month)
         break
     case 'februari':
-        console.log( 'februari heeft 28/29 dagen.')
-        break
-    case 'maart':
-        console.log( 'maart heeft 31 dagen.')
+        console.log('Er zijn 28/29 dagen in ' + month)
         break
     case 'april':
-        console.log( 'april heeft 30 dagen.')
-        break
-    case 'mei':
-        console.log( 'mei heeft 31 dagen.')
-        break
     case 'juni':
-        console.log( 'juni heeft 30 dagen.')
-        break
-    case 'juli':
-        console.log( 'juli heeft 31 dagen.')
-        break
-    case 'augustus':
-        console.log( 'augustus heeft 31 dagen.')
-        break
     case 'september':
-        console.log( 'september heeft 30 dagen.')
-        break
-    case 'oktober':
-        console.log( 'oktober heeft 31 dagen.')
-        break
     case 'november':
-        console.log( 'november heeft 30 dagen.')
+        console.log('Er zijn 30 dagen in ' + month)
         break
-    case 'december':
-        console.log( 'december heeft 31 dagen.')
-        break
+
     default:
         console.log('Vul een geldig maand in. ')
 }
